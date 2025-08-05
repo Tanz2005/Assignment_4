c463c0487ca13e16f0d806a81112f83691905b00
Tanishq <tanishq02022005@gmail.com>
2025-08-05 17:37:12 +0530

    Initial commit

---
+++ Assignment 4/Task 1.py
@@ -0,0 +1,8 @@
+try:
+    with open("sample.txt", "r") as file:
+        print("Reading file content:")
+        lines = file.readlines()
+        for i, line in enumerate(lines, start=1):
+            print(f"Line {i}: {line.strip()}")
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
